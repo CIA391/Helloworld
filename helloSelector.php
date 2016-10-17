e86a4d6e87193ea0600b801c3b6c5427aaddc657
1407746 <a>
2016-10-17 14:03:38 +0100

    Test version 4

--- helloSelector.php
+++ helloSelector.php
@@ -6,9 +6,9 @@
 </head>
 <body>
 <h1>Where would you like yo say hello?</h1>
-<a href="http://1407746.azurewebsites.net/helloPrinter.php">Earth</a> <br>
-<a href="http://1407746.azurewebsites.net/helloPrinter.php">Mars</a> <br>
-<a href="http://1407746.azurewebsites.net/helloPrinter.php">Uranus</a>
+<a href="http://1407746.azurewebsites.net/helloPrinter.php?planet=earth">Earth</a> <br>
+<a href="http://1407746.azurewebsites.net/helloPrinter.php?planet=mars">Mars</a> <br>
+<a href="http://1407746.azurewebsites.net/helloPrinter.php?planet=uranus">Uranus</a>
 <?php
 ?>
 </body>
