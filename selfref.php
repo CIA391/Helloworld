89fc5db0ff223cd2643f0e9e7789594f6dcb1308
1407746 <a>
2016-10-24 15:10:15 +0100

    Test version 4

--- selfref.php
+++ selfref.php
@@ -8,24 +8,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+echo "here";
+?>
 
-    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
-        ?>
-        <form action="selfref.php" method="post"> 
-            <label>Forename</label> 
-            <input type="text" name="forename"> 
-            <label>Surname</label> 
-            <input type="text" name="surname"> 
-            <p><input type="submit" value="Submit"></p> 
-        </form>
 
-    <?}  
-    elseif ($_SERVER['REQUEST_METHOD'] === 'POST'){ 
-    // execute if requested using HTTP POST Method 
-        echo "hello";
-    } 
-    else { 
-    // this is impossible 
-    }  ?> 
 </body> 
 </html>
