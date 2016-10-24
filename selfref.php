3a61174d36ab480c7733a5960569d4bf66d6ef21
1407746 <a>
2016-10-24 15:00:53 +0100

    Test version 4

---
+++ selfref.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+    <title>selfreference</title> 
+</head> 
+<body> 
+<?php 
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        ?>
+        <form action="<? echo "$_SERVER['PHP_SELF']"; ?>" method="post"> 
+            <label>Forename</label> 
+            <input type="text" name="forename"> 
+            <label>Surname</label> 
+            <input type="text" name="surname"> 
+            <p><input type="submit" value="Submit"></p> 
+        </form>
+        <?
+    } 
+    elseif ($_SERVER['REQUEST_METHOD'] === 'POST'){ 
+// execute if requested using HTTP POST Method 
+    echo "hello";
+    } 
+else { 
+// this is impossible 
+}  ?> 
+</body> 
+</html>
