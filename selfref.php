842c50e1ed4a0ea325bfe0edb175e2f35c739766
1407746 <a>
2016-10-24 15:34:21 +0100

    Test version 4

--- selfref.php
+++ selfref.php
@@ -5,29 +5,25 @@
 </head> 
 <body> 
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET') {}
-?>
-    <form action="<? echo "$_SERVER['PHP_SELF']"; ?>" method="post">
+if ($_SERVER['REQUEST METHOD'] === 'GET') {
+    ?>
+    <form action="<? echo $_SERVER['PHP_SELF']; ?>" method="post">
         <label>Forename</label> 
         <input type="text" name="forename"> 
         <label>Surname</label> 
         <input type="text" name="surname"> 
         <p><input type="submit" value="Submit"></p> 
     </form>
-<?
-} 
-elseif ($_SERVER['REQUEST_METHOD'] === 'POST'){
+    <?
+}
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $forename = $_POST["forename"];
     $surname = $_POST["surname"];
     print("<h1>Hello {$forename} {$surname}</h1>");
-} 
-    else { 
+}
+else {
     // this is impossible 
-    } 
+}
 ?> 
 </body> 
 </html>
