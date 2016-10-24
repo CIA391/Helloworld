576d093c4a302adc14530e7ef95c3217f5e4aa27
1407746 <a>
2016-10-24 16:27:56 +0100

    Test version 4

--- selfref.php
+++ selfref.php
@@ -1,27 +1,28 @@
-<!DOCTYPE html> 
-<html>
-<head> 
-    <title>selfreference</title> 
-</head> 
-<body> 
-<?php if ($_SERVER['REQUEST_METHOD'] === 'GET') { ?>
+<!DOCTYPE html>
+    <html>
+    <head>
+        <title>self</title>
+    </head>
+    <body>
+    <?php
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') { ?>
     <form action="selfref.php" method="post">
         <label>Forename</label>
          <input type="text" name="forename">
-         <label>Surname</label> 
+        <label>Surname</label> 
         <input type="text" name="surname"> 
         <p><input type="submit" value="Submit"></p> 
     </form>
     <?
-}
-elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $forename = $_POST["forename"];
-    $surname = $_POST["surname"];
-    print("<h1>Hello {$forename} {$surname}</h1>");
-}
-else {
-    // this is impossible 
-}
-?> 
-</body> 
+    }
+    elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $forename = $_POST["forename"];
+        $surname = $_POST["surname"];
+        print("<h1>Hello {$forename} {$surname}</h1>");
+    }
+    else {
+        // this is impossible 
+    }
+    ?> 
+    </body>
 </html>
