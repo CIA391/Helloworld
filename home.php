7f0f6eb55a654b51ea7016e79dbd23c414ab9157
1407746 <a>
2016-10-31 14:53:44 +0000

    Test version 4

--- home.php
+++ home.php
@@ -1,11 +1,11 @@
 <?php
 
-include ("Helloworld/header.php");
+include ("header.php");
 
 echo "
 <main>
 <p>Welcome to the blog of Charles Babbage! In this blog you will see all of my insights and wonderful things</p>";
 
-include ("Helloworld/footer/php");
+include ("footer/php");
 
 ?>
