c2b21aa9f76f285caaafd0962b09286894a0097e
1407746 <a>
2016-10-31 15:04:31 +0000

    Test version 4

--- inc/scripts/home.php
+++ inc/scripts/home.php
@@ -1,11 +1,11 @@
 <?php
 
-include ("header.php");
+include ("scripts/header.php");
 
 echo "
 <main>
 <p>Welcome to the blog of Charles Babbage! In this blog you will see all of my insights and wonderful things</p>";
 
-include ("footer/php");
+include ("scripts/footer/php");
 
 ?>
