d4c701c28afb800138fa7eb43965f6180c551337
Chris <christopher.aiken@btinternet.com>
2016-11-07 15:09:07 +0000

    Test version 4

---
+++ Week7/locations.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1407746
+ * Date: 07/11/2016
+ * Time: 15:08
+ */
--- Week7/test1.php
+++ Week7/test1.php
@@ -10,4 +10,37 @@ $db = new mysqli (
 if ($db->connect_errno) {
     die ('Connection Failed :'.$db->connect_error );
 }
+
+//Start XML file, create parent node
+$dom - new DOMDocument("1.0");
+$node = $dom->createElement("markers");
+$parnode = $dom->appendChild($node);
+
+//Select all the rows n the markers table
+$query = "SELECT * FROM markers WHERE 1";
+$result = $db->query($query);
+if (!result) {
+    die('Nothing in result: ');
+}
+
+header("Content-type: text/xml");
+
+//Iterate through the rows, adding XML node for each
+
+while ($row = result->fetch_array()){
+    //ADD TO XML DOCUMENT NODE
+    $node = $dom->createElement("marker");
+    $newnode = $parnode->appendChild($node);
+    $newnode->setAttribute("name",$row['name']);
+    $newnode->setAttribute("address", $row['address']);
+    $newnode->setAttribute("lat", $row['lat']);
+    $newnode->setAttribute("lng", $row['lng']);
+    $newnode->setAttribute("type", $row['type']);
+}
+
+$result->close();
+$db->close();
+
+echo $dom->saveXML();
+
 ?>
