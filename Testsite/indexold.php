33f985aace03e43cb8d4c27f3a13b379f8303a9a
Christopher Aiken <christopher.aiken@btinternet.com>
2016-11-30 11:17:19 +0000

    Create indexold.php

---
+++ Testsite/indexold.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en"> 
+	<head>
+		<meta charset="utf-8"/>
+		<title>Animasave</title>
+		<link rel="stylesheet" type="text/css" href="css/indexold.css">
+	</head>
+
+	<body>
+		<!--Logo-->
+		<div id="Logo">
+        <img src="images/Chris.jpg" alt="chris.jpg" height="150" width="250">
+		</div>
+
+		<!--Page Name-->
+		<div id="Pagename">
+		<p>Chris's Collage Work HUB</p>
+		</div>
+
+		<!--Homepage Button -->
+		<div id="Infobutton">
+			<p>Pages</p>
+        <form>
+		<input class="button" onclick="window.location.href='test.html'" type="button" value="Test" />
+        </form>
+	<form>
+		<input class="button" onclick="window.location.href='map.php'" type="button" value="Map" />
+        </form>
+        <form>
+		<input class="button" onclick="window.location.href='Health.php'" type="button" value="Health" />
+        </form>
+        <form>
+		<input class="button" onclick="window.location.href='Clubs.php'" type="button" value="Clubs" />
+        </form>
+		</div>
+
+		<!--External link test -->
+		<div id="Locationbutton">
+        <form>
+		<input class="button" onclick="window.location.href='http://animasave.altervista.org/PHP%20Folder/PHP%20Index%20Page.php'" type="button" value="PHP Index Page" /> 
+		</form>
+		</div>
+
+		<!--Body Text-->
+		<div id="BodyText">
+		<p>
+		Test site.<br>
+		Please edit at will.</p>
+			<a class="twitter-timeline" href="https://twitter.com/TwitterDev">Tweets by TwitterDev</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		</div>
+<a class="twitter-timeline"
+  href="https://twitter.com/TwitterDev"
+  data-width="300"
+  data-height="300">
+Tweets by @TwitterDev
+</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+		<div id="footer">
+		<address>Made 28 November 2016<br>
+  		Website by Blackrabbit - Unicorns - .</address>
+		</div>
+	</body>
+</html>
