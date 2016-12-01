1a5910e74777131036e41d6766c7e2428e5db8d9
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 22:19:55 +0000

    Update add.php

--- Testing/add.php
+++ Testing/add.php
@@ -1,5 +1,12 @@
 
-
+<?php
+include("dbconnect.php");
+// test if connection was established, and print any errors
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}   
+$sql_query = "INSERT INTO news(name, email, headline, story, timestamp)VALUES('$name', '$email', '$headline', '$story', NOW())";
+?>
 <form action="displaySuperhero.php" method="post">
     Username:<input type="text" name="name"><br>
     Email:<input type="text" name="email"><br>
