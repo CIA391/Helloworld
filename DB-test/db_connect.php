528207c8dc5700583220f5f46fc180bbf95ff6a1
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 20:23:47 +0000

    Create db_connect.php

---
+++ DB-test/db_connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$db = new mysqli (
+    'us-cdbr-azure-southcentral-f.cloudapp.net',
+    'bf9afe7c1df5c8',
+    '5d557954',
+'acsm_0dd8805538e55e7'
+);
+
+if (!$db) {
+    die('Connect Error: ' . mysqli_connect_errno());
+}
