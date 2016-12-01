562bdd33f7bde8c6b2c1823746c5d5b8f888006c
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 22:30:21 +0000

    Update add.php

--- Testing/add.php
+++ Testing/add.php
@@ -4,6 +4,19 @@ include("dbconnect.php");
 if (!$db) {
     die('Connect Error: ' . mysqli_connect_errno());
 }   
+
+$query = "INSERT INTO news(name, email, headline, story, timestamp)VALUES('$name', '$email', '$headline', '$story', NOW())";
+$result = @mysql_query($query);
+if(!$result){
+   echo('Error adding news: ' . $mysql_error());
+   exit();
+}else{
+mysql_close($link);
+echo('Success!<br><a href="add.php">Click here</a> to add more news.<br><a href="edit.php">Click here</a> to edit news.<br><a href="../index.php">Click here</a> to return to the main page.');
+}
+
+
+
 $sql_query = "INSERT INTO news(name, email, headline, story, timestamp)VALUES('$name', '$email', '$headline', '$story', NOW())";
 ?>
 <!--<form name="form1" method="post" action="<? echo $PHP_SELF; ?>"-->
