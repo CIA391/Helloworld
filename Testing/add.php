96eca2def72a9166f15d0000a21e2bf580e25564
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 19:51:16 +0000

    Update add.php

--- Testing/add.php
+++ Testing/add.php
@@ -1,3 +1,32 @@
+<html>
+<head>
+<title>Add News</title>
+<meta http-equiv="Content-Type" content="text/html; charset="iso"-8859-1">
+</head>
+<body>
+<?
+if(isset($add_n)){
+   $link = @mysql_connect(localhost, username, password);
+   if(!$link){
+      echo('Error connecting to the database: ' . $mysql_error());
+      exit();
+   }
+   $db = @mysql_selectdb('mydatabase');
+   if(!$db){
+      echo('Error selecting database: ' . $mysql_error());
+      exit();
+   }
+   $query = "INSERT INTO news(name, email, headline, story, timestamp)VALUES('$name', '$email', '$headline', '$story', NOW())";
+   $result = @mysql_query($query);
+   if(!$result){
+      echo('Error adding news: ' . $mysql_error());
+      exit();
+   }else{
+   mysql_close($link);
+   echo('Success!<br><a href="add.php">Click here</a> to add more news.<br><a href="edit.php">Click here</a> to edit news.<br><a href="../index.php">Click here</a> to return to the main page.');
+   }
+}else{
+?>
 <form name="form1" method="post" action="<? echo $PHP_SELF; ?>">
   <table width="50%" border="0" cellspacing="0" cellpadding="0">
     <tr> 
@@ -28,3 +57,6 @@
     </tr>
   </table>
   </form>
+<? } ?>
+</body>
+</html>
