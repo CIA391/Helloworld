33d7bf0515d418a2b5bc4a9100b12aebcbaa4c03
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 21:12:49 +0000

    Update marvel.php

--- DB-test/marvel.php
+++ DB-test/marvel.php
@@ -5,12 +5,7 @@
  * Date: 18/11/2015
  * Time: 09:43
  */
-$db = new mysqli(
-    "eu-cdbr-azure-west-c.cloudapp.net",
-    "b0c2ff384f05bb",
-    "82b4d3a0",
-    "mjc7778db"
-);
+include("db_connect.php");
 // test if connection was established, and print any errors
 if (!$db) {
     die('Connect Error: ' . mysqli_connect_errno());
