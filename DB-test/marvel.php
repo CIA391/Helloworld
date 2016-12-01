6bbdaed01ade00303e86c2b2299ee5dc8f3dd1c7
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 21:28:10 +0000

    Update marvel.php

--- DB-test/marvel.php
+++ DB-test/marvel.php
@@ -21,7 +21,7 @@ $result = $db->query($sql_query);
 echo "<p><strong>All Movies: </strong>";
 while($row = $result->fetch_array()){
    // print out fields from row of data
-  echo $row['title']  . " | " . ['notes'];
+  echo $row['title']  . " | " . $row['notes'] . " | ";
 }
 echo "</p>";
 $result->close();
