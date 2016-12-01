4d19ddb8917561517bbf18383b55463cbf57b72b
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 21:58:27 +0000

    Update marvel.php

--- DB-test/marvel.php
+++ DB-test/marvel.php
@@ -1,29 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: michaelcrabb
- * Date: 18/11/2015
- * Time: 09:43
- */
 include("db_connect.php");
-// test if connection was established, and print any errors
 if (!$db) {
     die('Connect Error: ' . mysqli_connect_errno());
 }
 
-
-// create a SQL query as a string
 $sql_query = "SELECT * FROM marvelmovies";
-// execute the SQL query
 $result = $db->query($sql_query);
-// iterate over $result object one $row at a time
-// use fetch_array() to return an associative array
 echo "<p><strong>All Movies: </strong>";
 while($row = $result->fetch_array()){
-   // print out fields from row of data
   echo $row['title']  . " | " . $row['notes'] . "</p>";
 }
 echo "</p>";
 $result->close();
-   // close connection to database
    $db->close();
