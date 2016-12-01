673d9646a3b51295f4a02c142afeaab7f9470f4d
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 23:47:21 +0000

    Update view.php

--- Testing/view.php
+++ Testing/view.php
@@ -8,6 +8,8 @@ $result = $db->query($sql_query);
 echo "<p><strong>All news: </strong>";
 while($row = $result->fetch_array()){
 echo $row['id'] . $row['headline'] . $row['story'] . "</p>" . $row['name'] . $row['email'] . $row['timestamp'] . "</p>";
+    
+"News ID: " . $row['id'] . ", " . $row['headline'] . "</p>" . "Writer: " . $row['name'] . ", Time: " . $row['timestamp'] . "</p>" . $row['story'] . "</p>";
 }
 $result->close();
    $db->close();
