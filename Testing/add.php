49ea014371b8885eef092f14844648b4af48806a
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-01 22:45:49 +0000

    Update add.php

--- Testing/add.php
+++ Testing/add.php
@@ -9,12 +9,16 @@ include("dbconnect.php");
 if (!$db) {
     die('Connect Error: ' . mysqli_connect_errno());
 }   
-$sql_query = "INSERT INTO news(name, email, headline, story, timestamp)VALUES('$name', '$email', '$headline', '$story', NOW())";
-if(!$sql_query){
+//$sql_query = "INSERT INTO news(name, email, headline, story, timestamp)VALUES('$name', '$email', '$headline', '$story', NOW())";
+
+    
+$query = "INSERT INTO news(name, email, headline, story, timestamp)VALUES('$name', '$email', '$headline', '$story', NOW())";
+$result = @mysql_query($query);
+if(!$result){
    echo('Error adding news: ' . $mysql_error());
    exit();
 }else{
-mysql_close($bd);
+mysql_close($db);
 echo('Success!<br><a href="add.php">Click here</a> to add more news.<br><a href="edit.php">Click here</a> to edit news.<br><a href="../index.php">Click here</a> to return to the main page.');
 }
 ?>
