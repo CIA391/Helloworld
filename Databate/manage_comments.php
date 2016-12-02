8cc4a70564a08edfb00fcf57accc888af3841584
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-02 00:53:29 +0000

    Update manage_comments.php

--- Databate/manage_comments.php
+++ Databate/manage_comments.php
@@ -2,7 +2,7 @@
 
 if( $_POST )
 {
-	$con = include("dbconnect.php");
+	$con = 'us-cdbr-azure-southcentral-f.cloudapp.net', 'bf9afe7c1df5c8', '5d557954');
 	if (!$con)
   {
     die('Could not connect: ' . mysql_error());
