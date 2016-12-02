b75467dbc8f995d86c5ca6009fd5f89785522436
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-02 00:48:37 +0000

    Create page2.php

---
+++ Databate/page2.php
@@ -0,0 +1,9 @@
+<? include("manage_comments.php"); ?>
+
+<h1>This is page1.php</h1>
+
+<div><a href='page1.php?id=1'>Click here</a> to go to page1.php</div>
+
+<div style='margin:20px; width:100px; height:100px; background:blue;'></div>
+
+<? include("formcode.php"); ?>
