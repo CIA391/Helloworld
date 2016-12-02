5c69df928f3932b13c2d0d7b7d25d1cdda833afb
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-02 01:11:05 +0000

    Delete page1.php

--- Databate/page1.php
+++
@@ -1,9 +0,0 @@
-<? include("manage_comments.php"); ?>
-
-<h1>This is page1.php</h1>
-
-<div><a href='page2.php?id=2'>Click here</a> to go to page2.php</div>
-
-<div style='margin:20px; width:100px; height:100px; background:blue;'></div>
-
-<? include("formcode.php"); ?>
