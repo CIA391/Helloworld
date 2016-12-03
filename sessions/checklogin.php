593e9059c52dc4d8a74d78e9f78672f84dcb9d1e
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-03 22:48:05 +0000

    Update checklogin.php

--- sessions/checklogin.php
+++ sessions/checklogin.php
@@ -1,6 +1,6 @@
 <?php
-$username = $_POST["name"];
-$password = $_POST["password"];
+$username = mysqli_real_escape_string ($_POST["name"]);
+$password = mysqli_real_escape_string ($_POST["password"]);
 if ($username =="username" && $password=="password")
 {
     session_start();
