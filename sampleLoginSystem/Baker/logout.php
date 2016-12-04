cc8ae6a82e4eb79a3fa7922f16f17ed32a480720
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 20:46:38 +0000

    Update logout.php

--- sampleLoginSystem/Baker/logout.php
+++ sampleLoginSystem/Baker/logout.php
@@ -1,9 +1,9 @@
 <?
+//This code removes the users session. Do not touch.
 session_start();
 if (isset($_SESSION['username']))
 {
     unset($_SESSION['username']);
 }
-
 header("location:index.php");
 ?>
