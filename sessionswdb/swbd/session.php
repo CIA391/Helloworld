31fc1ed55265451bc63924955cecefd6098b23d0
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 14:33:27 +0000

    Update session.php

--- sessionswdb/swbd/session.php
+++ sessionswdb/swbd/session.php
@@ -1,16 +1,1 @@
-<?php
-   include('DBCONNECT.php');
-   session_start();
-   
-   $user_check = $_SESSION['login_user'];
-   
-   $ses_sql = mysqli_query($db, = '$user_check');
-   
-   $row = mysqli_fetch_array($ses_sql,MYSQLI_ASSOC);
-   
-   $login_session = $row['username'];
-   
-   if(!isset($_SESSION['login_user'])){
-      header("location:login.php");
-   }
-?>
+
