124994ab9fd02f086331dcb868420ff88370b1ec
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 17:45:41 +0000

    Create signup.php

---
+++ login/T3/signup.php
@@ -0,0 +1,7 @@
+<html>
+        <form action="signup.php" method="post">
+              Username:<input type="text" name="n"><br />
+              Password:<input type="password" name="p"><br />
+              <input type="submit">
+        </form>
+ </html>
