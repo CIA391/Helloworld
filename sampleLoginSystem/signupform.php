5a2dd37dbbbf62045525263572a65cf9506134f9
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 19:36:39 +0000

    Update and rename signup.php to signupform.php

--- sampleLoginSystem/signupform.php
+++ sampleLoginSystem/signupform.php
@@ -2,14 +2,13 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login Form</title>
+    <title>Signup Form</title>
 </head>
 <body>
 
     <h1>Signup Form</h1>
-    <form method="post" action="signup2.php">
+    <form method="post" action="signup.php">
         <p><input type="text" name="username" value="" placeholder="Username"></p>
-        <p><input type="text" name="email" value="" placeholder="eMail"></p>
         <p><input type="password" name="password" value="" placeholder="Password"></p>
         <p><input type="password" name="passwordcheck" value="" placeholder="Confirm Password"></p>
         <p class="submit"><input type="submit" name="commit" value="Login"></p>
