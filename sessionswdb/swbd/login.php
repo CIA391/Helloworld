88f80a445c63b10ff3b58c0e191f7d6dd722bb9b
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 16:00:14 +0000

    Update login.php

--- sessionswdb/swbd/login.php
+++ sessionswdb/swbd/login.php
@@ -1,37 +1,27 @@
 <?php
-   include("DBCONNECT.php");
-      session_start();
+	include("DBCONNECT.php");
+     	session_start();
    
-   if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
+	if($_SERVER["REQUEST_METHOD"] == "POST") {
+	// username and password sent from form 
       
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+	$myusername = mysqli_real_escape_string($db,$_POST['username']);
+        $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
       
-      $sql = "SELECT * FROM admin WHERE username = '$myusername' && passcode = '$mypassword'";
-      $result = mysqli_query($db,$sql);
-      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
-      $active = $row['active'];
+        $sql = "SELECT * FROM admin WHERE username = '$myusername' && passcode = '$mypassword'";
+        $result = mysqli_query($db,$sql);
+        $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        $active = $row['active'];
       
-      if ($row['username'] == $myusername && $row['passcode'] == $mypassword ){
-	      echo "you got in ". $row['username'];
-         $_SESSION['login_user'] = $myusername;
+        if ($row['username'] == $myusername && $row['passcode'] == $mypassword ){
+		echo "you got in ". $row['username'];
+         	$_SESSION['login_user'] = $myusername;
          
-         header("location: welcome.php");
+         	header("Location: welcome.php");
 	   
-      //$count = mysqli_num_rows($result);
-      //echo "$count";
-      // If result matched $myusername and $mypassword, table row must be 1 row
-		
-      //if($count == 1) {
-         //session_register("$myusername");
-         //$_SESSION['login_user'] = $myusername;
-         
-         //header("location: welcome.php");
       }else {
-         $error = "Your Login Name or Password is invalid";
+         	$error = "Your Login Name or Password is invalid";
       }
-   }
 ?>
 <html>
    
