56c0443cfec748e88069e46278bedba06b999e03
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 14:17:21 +0000

    Update login.php

--- sessionswdb/swbd/login.php
+++ sessionswdb/swbd/login.php
@@ -1,26 +1,26 @@
 <?php
    include("DBCONNECT.php");
-   session_start();
+      session_start();
    
    if($_SERVER["REQUEST_METHOD"] == "POST") {
       // username and password sent from form 
       
-      $myusername = mysqli_real_escape_string($db,$_POST['username']);	
-      $mypassword = mysqli_real_escape_string($db,$_POST['password']);       	
-	   
-      $sql = "SELECT * FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
       $active = $row['active'];
-      	
+      
       $count = mysqli_num_rows($result);
-      	echo "$count";
+      
       // If result matched $myusername and $mypassword, table row must be 1 row
 		
-      if('$count' == 1) {
+      if($count == 1) {
          session_register("myusername");
-         $_SESSION['login_user'] = '$myusername';
-        
+         $_SESSION['login_user'] = $myusername;
+         
          header("location: welcome.php");
       }else {
          $error = "Your Login Name or Password is invalid";
