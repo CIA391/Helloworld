0c7375d1a478542f7ea55ebfacad4106358dfbfc
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 21:05:25 +0000

    Update signupform.php

--- sampleLoginSystem/Baker/signupform.php
+++ sampleLoginSystem/Baker/signupform.php
@@ -2,14 +2,14 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login Form</title>
+    <title>Signup Form</title>
 </head>
 <body>
     <h1>Signup Form</h1>
     <form method="post" action="signup.php">
-        <p><input type="text" name="username" value="" placeholder="Username"></p>
-        <p><input type="password" name="password" value="" placeholder="Password"></p>
-        <p><input type="password" name="passwordcheck" value="" placeholder="Confirm Password"></p>
+        <p><input type="text" name="username" value="" placeholder="Username please"></p>
+        <p><input type="password" name="password" value="" placeholder="Password please"></p>
+        <p><input type="password" name="passwordcheck" value="" placeholder="Confirm Password please"></p>
         <p class="submit"><input type="submit" name="commit" value="Login"></p>
     </form>
 </body>
