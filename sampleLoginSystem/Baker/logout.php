27b60a25289a7468f220f59562c81daf386a9251
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 22:04:00 +0000

    Update logout.php

--- sampleLoginSystem/Baker/logout.php
+++ sampleLoginSystem/Baker/logout.php
@@ -1,9 +1,17 @@
 <?
-//This code removes the users session. Do not touch.
+//This code removes the users session. And also clears other sessions that are in the way.
 session_start();
 if (isset($_SESSION['username']))
 {
     unset($_SESSION['username']);
+}
+if (isset($_SESSION['Signupfail']))
+{
+    unset($_SESSION['Signupfail']);
+}
+if (isset($_SESSION['Loginfail']))
+{
+    unset($_SESSION['Loginfail']);
 }
 header("location:index.php");
 ?>
