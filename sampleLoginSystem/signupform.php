bb7d05bad1dcaedf9fb464418fa80ba4299588a6
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 19:39:01 +0000

    Update signupform.php

--- sampleLoginSystem/signupform.php
+++ sampleLoginSystem/signupform.php
@@ -5,15 +5,12 @@
     <title>Signup Form</title>
 </head>
 <body>
-
     <h1>Signup Form</h1>
     <form method="post" action="signup.php">
-        <p><input type="text" name="username" value="" placeholder="Username"></p>
-        <p><input type="password" name="password" value="" placeholder="Password"></p>
-        <p><input type="password" name="passwordcheck" value="" placeholder="Confirm Password"></p>
+        <p><input type="text" name="username" value="" placeholder="Username here please"></p>
+        <p><input type="password" name="password" value="" placeholder="Password here please"></p>
+        <p><input type="password" name="passwordcheck" value="" placeholder="Confirm Password please"></p>
         <p class="submit"><input type="submit" name="commit" value="Login"></p>
     </form>
-
-
 </body>
 </html>
