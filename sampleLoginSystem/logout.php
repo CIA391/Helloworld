067ac938c827268beb9e7eed60fa9a40ec0c4202
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 19:34:15 +0000

    Update logout.php

--- sampleLoginSystem/logout.php
+++ sampleLoginSystem/logout.php
@@ -1,9 +1,9 @@
 <?
+//Code to logout. This logs the user out completely
 session_start();
 if (isset($_SESSION['username']))
 {
     unset($_SESSION['username']);
 }
-
 header("location:index.php");
 ?>
