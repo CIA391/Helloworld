e375654eec95666c451d3b0988ce0c5c97b86c02
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 22:38:01 +0000

    Update signup.php

--- sampleLoginSystem/Baker/signup.php
+++ sampleLoginSystem/Baker/signup.php
@@ -6,27 +6,10 @@ $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 $passwordcheck = $_POST["passwordcheck"];
 
-//This checks if there is any spaces in the user entered data
-if (strpos($myusername, ' ') !== false) {
-    $userspace = 'true';
-}
-if (strpos($mypassword, ' ') !== false) {
-    $passspace = 'true';
-}
-if (strpos($passwordcheck, ' ') !== false) {
-    $pass2space ='true';
-}
 
 
 //This checks if the text is blank or not
 if ($mypassword=="" || $mypassword=="" || $passwordcheck==""){
-    session_start();
-    $_SESSION['Signupfail'];
-    header("location:signupform.php");
-}
-
-//This tests to see if there is any spaces in the text
-if($userspace=='true' || $passspace=='true' || $pass2space=='true')
     session_start();
     $_SESSION['Signupfail'];
     header("location:signupform.php");
