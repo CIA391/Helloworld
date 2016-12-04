7d5562be9a941fc1f78f69d3cf61395ce4249bf6
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 19:32:27 +0000

    Update index.php

--- sampleLoginSystem/index.php
+++ sampleLoginSystem/index.php
@@ -7,16 +7,11 @@
 <body>
 
 <?
-//Opens up a session
 session_start();
-
 include ("db_connect.php");
 
-//Check to see if the 'username' session variable is set.
-
-if (isset($_SESSION['username'])) //SESSION DOES EXIST
+if (isset($_SESSION['username']))
 {
-
     echo "<p>Hello " . $_SESSION['username'] . "</p>";
     $sql = "SELECT * FROM users WHERE username='". $_SESSION['username'] . "'";
     $result = $db->query($sql);
@@ -26,26 +21,21 @@ if (isset($_SESSION['username'])) //SESSION DOES EXIST
     }
     ?>
     <a href="logout.php">Logout</a>
+    
     <?
-
-
 }
-else //SESSION DOESNT EXIST
+else
 {
-
 ?>
-
-    <h1>My Login Form</h1>
+    <h1>Login</h1>
     <form method="post" action="checklogin.php">
-    <p><input type="text" name="username" value="" placeholder="Username or Email"></p>
+    <p><input type="text" name="username" value="" placeholder="Username"></p>
     <p><input type="password" name="password" value="" placeholder="Password"></p>
     <p class="submit"><input type="submit" name="commit" value="Login"></p>
     </form>
     <a href="signup.php">Signup</a>
-
 <?
 }
 ?>
-
 </body>
 </html>
