8466f2ffeee27694deb995db90a7782a47c2fe54
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 13:43:31 +0000

    Update test.php

--- sessionswdb/test.php
+++ sessionswdb/test.php
@@ -6,8 +6,10 @@
       // username and password sent from form 
       
       $myusername = mysqli_real_escape_string($db,$_POST['username']);
+	   echo "$myusername";
       $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
-      
+      	echo "$mypassword";
+	   
       $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
       $result = mysqli_query($db,$sql);
       $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
