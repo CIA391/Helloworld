551ea452d4a3a1aa9ec13362ff600a62ea97afed
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 22:36:22 +0000

    Update signup.php

--- sampleLoginSystem/Baker/signup.php
+++ sampleLoginSystem/Baker/signup.php
@@ -8,28 +8,32 @@ $passwordcheck = $_POST["passwordcheck"];
 
 //This checks if there is any spaces in the user entered data
 if (strpos($myusername, ' ') !== false) {
-    $userspace == 'true';
+    $userspace = 'true';
 }
 if (strpos($mypassword, ' ') !== false) {
-    $passspace == 'true';
+    $passspace = 'true';
 }
 if (strpos($passwordcheck, ' ') !== false) {
-    $pass2space =='true';
+    $pass2space ='true';
 }
 
 
-//This checks if the password is 100% what the user typed
+//This checks if the text is blank or not
 if ($mypassword=="" || $mypassword=="" || $passwordcheck==""){
     session_start();
     $_SESSION['Signupfail'];
     header("location:signupform.php");
 }
-//elseif($userspace=='true' || $passspace=='true' || $pass2space=='true')
-//    session_start();
-//    $_SESSION['Signupfail'];
-//    header("location:signupform.php");
-//}
-elseif($mypassword==$passwordcheck)
+
+//This tests to see if there is any spaces in the text
+if($userspace=='true' || $passspace=='true' || $pass2space=='true')
+    session_start();
+    $_SESSION['Signupfail'];
+    header("location:signupform.php");
+}
+
+//This checks if the password is 100% what the user typed
+if($mypassword==$passwordcheck)
 {
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";
 
