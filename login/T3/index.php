94853548cf0e474ff6b15f8dd5f17710dd817d50
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 16:56:39 +0000

    Create index.php

---
+++ login/T3/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login Page</title>
+</head>
+<body>
+<form action="checklogin.php" method="post">
+    Name:<br>
+    <input type="text" name="username">
+    <br>
+    Password:<br>
+    <input type="password" name="password">
+    <br><br>
+    <input type="submit" value="Submit">
+</form>
+</body>
+</html>
