a4e4614eb4ac86dfabbc5eca5ac3f1b6db72c11a
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 17:03:51 +0000

    Update login.php

--- login/T3/login.php
+++ login/T3/login.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+include 'DBCONNECT.php';
 if(isset($_POST['submit'])) {
   $myusername = $_POST['username'];
   $mypassword = $_POST['password'];
@@ -9,6 +9,34 @@ if(isset($_POST['submit'])) {
   $mypassword = stripcslashes($mypassword);
   $myusername = mysqli_real_escape_string($myusername);
   $mypassword = mysqli_real_escape_string($mypassword);
-  echo $myusername. '----' .$password;
+  chckusername($username, $password);
+	}
+
+	function chckusername($username, $password){
+		include_once 'sql.php';
+		$check = "SELECT * FROM users WHERE username='$username'";
+		$check_q = mysql_query($check) or die("<div class='loginmsg'>Error on checking Username<div>");
+		if (mysql_num_rows($check_q) == 1) {
+			chcklogin($username, $password);
+		}
+		else{
+			echo "<div id='loginmsg'>Wrong Email</div>";
+		}
+	}
+	function chcklogin($username, $password){
+		$login = "SELECT * FROM users WHERE username='$username'  and password='$password'";
+		$login_q = mysql_query($login) or die('Error on checking Username and Password');
+		// Mysql_num_row is counting table row
+		if (mysql_num_rows($login_q) == 1){
+			echo "<div id='loginmsg'> Logged in as $username </div>"; 
+			$_SESSION['username'] = $username;
+			header('Location: member.php');
+		}
+		else {
+			echo "<div id='loginmsg'>Wrong Password </div>"; 
+			//header('Location: login-problem.php');
+		}
+	}
+?>
 }
 ?>
