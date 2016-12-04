c964bbfde4891e86a8153eaca9506f0135eb53f0
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 16:30:47 +0000

    Update login.php

--- login/T2/login.php
+++ login/T2/login.php
@@ -6,18 +6,19 @@
 	
 	$myusername = stripslashes($myusername);
 	$mypassword = stripslashes($mypassword);
-	
+	echo "$myusername";
+	echo "$mypassword";
 	$query = "SELECT * FROM admin WHERE username='$myusername' and password='$mypassword'";
 	$result = mysql_query($query);
 	$count = mysql_num_rows($result);
 	
 	mysql_close();
 	
-	if($count==1){
-		$seconds = 5 + time();
-		setcookie(loggedin, date("F jS - g:i a"), $seconds);
-		header("location:login_success.php");
-	}else{
-		echo 'Incorrect Username or Password';
-	}
+	//if($count==1){
+		//$seconds = 5 + time();
+		//setcookie(loggedin, date("F jS - g:i a"), $seconds);
+		//header("location:login_success.php");
+	//}else{
+		//echo 'Incorrect Username or Password';
+	//}
 ?>
