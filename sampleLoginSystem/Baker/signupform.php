29f2c3eabc23a98dc0ffd61f51f788d65fa0c925
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-04 21:00:51 +0000

    Update and rename signup.php to signupform.php

--- sampleLoginSystem/Baker/signupform.php
+++ sampleLoginSystem/Baker/signupform.php
@@ -6,7 +6,7 @@
 </head>
 <body>
     <h1>Signup Form</h1>
-    <form method="post" action="signup2.php">
+    <form method="post" action="signup.php">
         <p><input type="text" name="username" value="" placeholder="Username"></p>
         <p><input type="password" name="password" value="" placeholder="Password"></p>
         <p><input type="password" name="passwordcheck" value="" placeholder="Confirm Password"></p>
