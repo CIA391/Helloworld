5b32059d9c5993fedaadd373174ca3987bb40905
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 13:28:29 +0000

    Update checklogin.php

--- sampleLoginSystem/Baker2/checklogin.php
+++ sampleLoginSystem/Baker2/checklogin.php
@@ -3,15 +3,19 @@ include ("db_connect.php");
 //information from the index form
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
+echo "$myusername";
+echo "$mypassword";
 
-//Saves the username as it
-$susername = $myusername;
 
 //checking to see if any usernames and password pairs match any in the database
-//$myusername = stripslashes($myusername);
-//$myusername = mysql_real_escape_string($myusername);
-//$mypassword = stripslashes($mypassword);
-//$mypassword = mysql_real_escape_string($mypassword);
+$myusername = stripslashes($myusername);
+$myusername = mysql_real_escape_string($myusername);
+$mypassword = stripslashes($mypassword);
+$mypassword = mysql_real_escape_string($mypassword);
+echo "$myusername";
+echo "$mypassword";
+die();
+
 $sql = "SELECT * FROM users WHERE username ='". $myusername ."' and password ='". $mypassword . "'";
 //The following code checks to see if any match
 $result = $db->query($sql);
