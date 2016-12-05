807565e136b8c3e9a77322f54333b3b397a8eb35
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 11:15:57 +0000

    Update signupform.php

--- sampleLoginSystem/Baker/signupform.php
+++ sampleLoginSystem/Baker/signupform.php
@@ -7,22 +7,6 @@
 <body>
     <h1>Signup Form</h1>
     <?
-    session_start();
-    $jives = $_SESSION['Signupfail'];
-    echo $jives;
-    
-    if (isset($_SESSION['Signupfail']))
-    {
-        if ($jives == "Fail1"){
-            echo "<p><font color='red'>Please ensure you dont use spaces in your username or password.</font></p>";
-        } elseif ($jives == "Fail2"){
-            echo "<p><font color='red'>Please ensure you fill all the fields.</font></p>";
-        }elseif ($jives == "Fail3"){
-            echo "<p><font color='red'>Please ensure the password fields match.</font></p>";
-        }elseif ($jives == "Fail4"){
-            echo "<p><font color='red'>Username already taken.</font></p>";
-        }
-    }
     //get test
     if (isset($_GET["empty"])){
         echo "<p><font color='red'>Please ensure you dont use spaces in your username or password.</font></p>";
