4772482b2be4562ea0314f2a8b48e39ea198b5bc
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 15:07:35 +0000

    Update signup.php

--- sampleLoginSystem/Baker2/signup.php
+++ sampleLoginSystem/Baker2/signup.php
@@ -4,14 +4,12 @@ include ("db_connect.php");
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 $passwordcheck = $_POST["passwordcheck"];
-
 $myusername = stripslashes($myusername);
 $myusername = mysqli_real_escape_string($db, $myusername);
 $mypassword = stripslashes($mypassword);
 $mypassword = mysqli_real_escape_string($db, $mypassword);
 $passwordcheck = stripslashes($passwordcheck);
 $passwordcheck = mysqli_real_escape_string($db, $passwordcheck);
-
 //This checks if there is any spaces in the user entered data
 if (strpos($myusername, ' ') !== false) {
     $userspace = 'true';
@@ -34,13 +32,12 @@ if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     die();
 }
 //This checks to see if the username is taken or not.
-$dup = "SELECT username FROM users WHERE username='$myusername'";
+$dup = mysql_query("SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysql_fetch_assoc($dup);
 if(mysql_num_rows($dup) >0){
     header("location:signupform.php?dup=1");
     die();
 }
-
 //This checks if the password is 100% what the user typed
 if($mypassword==$passwordcheck)
 {
