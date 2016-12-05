6af363ec129d9cdbfe377d8e15b7d7fcf674dd82
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 11:23:53 +0000

    Update signup.php

--- sampleLoginSystem/Baker/signup.php
+++ sampleLoginSystem/Baker/signup.php
@@ -1,11 +1,9 @@
 <?php
 include ("db_connect.php");
-
 //This is the fields from the signup form
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 $passwordcheck = $_POST["passwordcheck"];
-
 //This checks if there is any spaces in the user entered data
 if (strpos($myusername, ' ') !== false) {
     $userspace = 'true';
@@ -16,37 +14,36 @@ if (strpos($mypassword, ' ') !== false) {
 if (strpos($passwordcheck, ' ') !== false) {
     $pass2space ='true';
 }
-
 //This tests to see if there is any spaces in the text
 if($userspace=='true' || $passspace=='true' || $pass2space=='true') {
+    //session_start();
+    //$_SESSION['Signupfail'] = "Fail1";
     header("location:signupform.php?space=1");
     die();
 }
-
 //This checks to see if the fields are empty or not.
 if(empty($myusername) || empty($mypassword) || empty($passwordcheck))
     {
+    //session_start();
+    //$_SESSION['Signupfail'] = "Fail2";
     header("location:signupform.php?empty=1");
     die();
 }
-
 //This checks to see if the username is taken or not.
 $dup = mysql_query("SELECT username FROM users WHERE username='$myusername'");
 $userchecker = mysql_fetch_assoc($dup);
 if(mysql_num_rows($dup) >0){
-    //session_start();
-    //$_SESSION['Signupfail'] = "Fail4";
-    header("location:signupform.php?duplicate=1");
+    session_start();
+    $_SESSION['Signupfail'] = "Fail4";
+    header("location:signupform.php");
     die();
 }
-
 $myusername = mysql_real_escape_sequence($_GET['username']);
 $query = "SELECT username FROM users WHERE username = '$myusername'";
 if ($myusername == $userchecker['username']) {
     echo "Not avaifddd";
     die();
 }
-
 $results = @mysql_query($query);
 if(mysql_num_rows($results) > 0)
 {
@@ -54,7 +51,6 @@ if(mysql_num_rows($results) > 0)
   echo "Error: Username already taken.";
     die();
 }
-
 //This checks if the password is 100% what the user typed
 if($mypassword==$passwordcheck)
 {
@@ -65,21 +61,18 @@ if($mypassword==$passwordcheck)
     $passwordcheck = stripslashes($passwordcheck);
     $passwordcheck = mysql_real_escape_string($passwordcheck);
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";
-
     if (mysqli_query($db, $sql)) {
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($db);
     }
-
     session_start();
     $_SESSION['username'] = $myusername;
     header("location:index.php");
-
     $sql = "INSERT INTO users (username, password, userType) VALUES ('". $myusername ."', '" .$mypassword."', 'reader')";
 } else {
     session_start();
     $_SESSION['Signupfail'] = "Fail3";
-    header("location:signupform.php?match=1");
+    header("location:signupform.php");
     die();
 }
 ?>
