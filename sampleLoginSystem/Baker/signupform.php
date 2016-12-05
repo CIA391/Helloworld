0be78c9993b5e706c81dbd559b7fc6fc240cc0f8
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 11:29:53 +0000

    Update signupform.php

--- sampleLoginSystem/Baker/signupform.php
+++ sampleLoginSystem/Baker/signupform.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,28 +15,15 @@
     if (isset($_SESSION['Signupfail']))
     {
         if ($jives == "Fail1"){
-            //echo "<p><font color='red'>Please ensure you dont use spaces in your username or password.</font></p>";
+            echo "<p><font color='red'>Please ensure you dont use spaces in your username or password.</font></p>";
         } elseif ($jives == "Fail2"){
-            //echo "<p><font color='red'>Please ensure you fill all the fields.</font></p>";
+            echo "<p><font color='red'>Please ensure you fill all the fields.</font></p>";
         }elseif ($jives == "Fail3"){
             echo "<p><font color='red'>Please ensure the password fields match.</font></p>";
         }elseif ($jives == "Fail4"){
             echo "<p><font color='red'>Username already taken.</font></p>";
         }
     }
-    //get test
-    if (isset($_GET["empty"])){
-        echo "<p><font color='red'>Please ensure you dont use spaces in your username or password.</font></p>";
-    }    
-    if (isset($_GET["space"])){
-        echo "<p><font color='red'>Please ensure you fill all the fields.</font></p>";
-    }
-    if (isset($_GET["match"])){
-        echo "<p><font color='red'>Please ensure the password fields match.</font></p>";
-    }
-    if (isset($_GET["duplicate"])){
-        echo "<p><font color='red'>Please ensure the password fields match.</font></p>";
-    }  
     ?>
     <form method="post" action="signup.php">
         <!-- This is the form used for users to sign up -->
