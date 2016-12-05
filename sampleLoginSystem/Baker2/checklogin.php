51aefee30149597a45cf087954aed8dc95474a05
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 13:47:03 +0000

    Update checklogin.php

--- sampleLoginSystem/Baker2/checklogin.php
+++ sampleLoginSystem/Baker2/checklogin.php
@@ -1,11 +1,19 @@
 <?php
 include ("db_connect.php");
 //information from the index form
+
+if(isset($_POST["submit"])) {
+    if(empty($_POST["username"]) || empty($_POST["username"])){
+        $error = "Both fields are required.";
+    } else {
+
+
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 echo "$myusername";
 echo "$mypassword";
 
+if(empty($_POST["username"] ||
 
 //checking to see if any usernames and password pairs match any in the database
 $myusername = stripslashes($myusername);
@@ -15,7 +23,7 @@ $mypassword = mysql_real_escape_string($mypassword);
 echo "$myusername";
 echo "$mypassword";
 die();
-
+}
 $sql = "SELECT * FROM users WHERE username ='". $myusername ."' and password ='". $mypassword . "'";
 //The following code checks to see if any match
 $result = $db->query($sql);
