d94a720177a3b108e023e33066263d49379bea86
Christopher Aiken <christopher.aiken@btinternet.com>
2016-12-05 16:21:18 +0000

    Update checklogin.php

--- sampleLoginSystem/Baker2/checklogin.php
+++ sampleLoginSystem/Baker2/checklogin.php
@@ -9,6 +9,10 @@ error_reporting(E_ALL);
 $myusername = $_POST["username"];
 $mypassword = $_POST["password"];
 
+//checks
+echo "<p>Username is {$myusername}</p>
+<p>Password is {$mypassword}</p>";
+
 //Security checks
 $myusername = stripslashes($myusername);
 $myusername = mysqli_real_escape_string($db,$myusername);
@@ -21,6 +25,7 @@ $result = $db->query($sql);
 $checker = 0;
 while($row = $result->fetch_array()) {
     $checker = 1;
+    echo "<p>Checker run</p>";
 }
 
 $userType = "";
@@ -28,10 +33,12 @@ $userType = "";
 $boom = "SELECT usertype FROM users WHERE username ='". $myusername ."' and password ='". $mypassword . "'";
 $result = $db->query($boom);
 while($row = $result->fetch_array()){
+    echo "<p>Item found in database, setting $usertype</p>";
 $userType = $row['userType'];
 }   
   
 //delete me after fixed
+echo "<p>Printing Usertype</p>";
 echo $userType;
 
 //This deals with if any matched or not. And send the user back to the index page
